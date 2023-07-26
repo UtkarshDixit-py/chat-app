062722b005eb847c642347f36d0013f10893ce28
UtkarshDixit-py <utkarshdixit303@gmail.com>
2023-07-26 22:53:52 +0530

    adding functionalities

--- index.js
+++ index.js
@@ -1,30 +1,64 @@
-const path = require('path');
-const express = require('express');
-const http = require('http');
-const socketio = require('socket.io');
+const path = require("path");
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const formatMessage = require("./helper/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./helper/users");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-io.on('connect', socket => {
-    socket.emit('message','<-welcome->');
+io.on("connect", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
+
+    socket.emit("message", formatMessage("SERVER", "WELCOME"));
 
     //notifying when new user connects
-    socket.broadcast.emit('message','new user has joined the chat');
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage("SERVER", `${user.username} has joined the channel`)
+      );
 
-    //when user logs out of channel
-    socket.on('disconnect',()=>{
-        io.emit('message','someone has left the chat')
-    })
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
 
-    //listening for message in chat
-    socket.on('chatMessage',(msg)=>{
-        console.log(msg);
-    })
-    
+  //listening for message in chat
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  //when user logs out of channel
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage("SERVER", `${user.username} just left the chat`)
+      );
+
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
 });
 
 const PORT = process.env.PORT || 3002;
--- public/script.js
+++ public/script.js
@@ -1,16 +1,57 @@
-const chatForm = document.getElementById('chat-form')
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
+
+//getting username and room name
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
 const socket = io();
 
-socket.on('message',message=>{
-    console.log(message);
-}) 
+socket.emit("joinRoom", { username, room });
+
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
+socket.on("message", (message) => {
+  outputMessage(message);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
 
 //submit message
-chatForm.addEventListener('submit',(e)=>{
-     e.preventDefault();
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-     const msg = e.target.elements.msg.value;
+  const msg = e.target.elements.msg.value;
 
-     //emitting to server
-     socket.emit('chatMessage',msg);
-})
+  //emitting to server
+  socket.emit("chatMessage", msg);
+
+  e.target.elements.msg.value = "";
+});
+
+function outputMessage(msg) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="user-name">${msg.username}</p> <p class="text">${msg.text}</p>`;
+
+  document.querySelector(".chat-messages").appendChild(div);
+}
+
+document.getElementById("leave-btn").addEventListener("click", () => {
+  window.location = "../index.html";
+});
+
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+function outputUsers(users) {
+  userList.innerHTML = `
+   ${users.map((user) => `<li>${user.username}</li>`).join("")}
+   `;
+}
